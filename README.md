8d2b46e01df46421cabb1f19238f566f3b4c54e2
woddp <717563864@qq.com>
2019-03-14 19:47:39 +0800

    first commot

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# pronhub
+# yellow_moves
 
 ## Project setup
 ```
---
+++ src/components/common/crossScroll.vue
@@ -0,0 +1,49 @@
+<template>
+        <div class=" corssScroll" >
+            <md-scroll-view
+                    ref="scrollView"
+                    :scrolling-y="false"
+            >
+                <slot></slot>
+            </md-scroll-view>
+    </div>
+</template>
+<script>
+    import {ScrollView} from 'mand-mobile'
+    export default{
+        props:['wHeight'],
+        components:{
+            [ScrollView.name]: ScrollView,
+        },
+        data(){
+            return {
+            }
+        },
+        mounted(){
+
+        },
+        methods:{
+
+        }
+    }
+</script>
+<style>
+
+   .corssScroll .md-scroll-view{
+       height: auto !important;
+   }
+   .corssScroll-md-scroll-view{
+       display:flex;
+       align-items:center;
+   }
+   /*.corssScroll-scroll-view-list{*/
+        /*width:10000px;*/
+       /*flex-wrap: wrap;*/
+    /*}*/
+   .corssScroll-scroll-view-item{
+
+    }
+
+
+
+</style>
